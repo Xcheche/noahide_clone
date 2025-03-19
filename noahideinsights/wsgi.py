fdf4518bc334b3840096a00c919c645cbaa71fdb
xcheche <chekwubechukwu23@gmail.com>
2025-03-19 18:04:24 +0100

    fixed search button and added pagination

--- noahideinsights/wsgi.py
+++ noahideinsights/wsgi.py
@@ -14,3 +14,9 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "noahideinsights.settings")
 
 application = get_wsgi_application()
+
+
+
+
+
+app = application
